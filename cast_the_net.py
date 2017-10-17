7d3acc99b0d1841b78fd8a32d4ed774638b0c96f
Nick Weiner <bigweiner@ucdavis.edu>
2017-10-17 15:05:28 -0700

    Nearly finished cast_the_net, just needs to analyze all keggs instead of picking random ones.

--- cast_the_net.py
+++ cast_the_net.py
@@ -4,16 +4,35 @@
 
 import phytozomedler
 import random
+org_id = '447' # arabidopsis
+path = 'kegg_{}.TSV'.format(org_id)
+try:
+    with open(path) as f:
+        data = f.readlines()
+except OSError:
+    base_species_query = \
+        phytozomedler.make_my_xml([['organism_id', org_id], ['has_ko',
+                                   'excluded="0"']], ['ko_id'],
+                                  'TSV', 'af')
+# 'kog_id', 'kog_desc', 'kegg_enzyme_id', 'kegg_enzyme_desc',
+    data_f = phytozomedler.get_results(base_species_query)
+    data = data_f.split('\n')
+    sorted_data = '\n'.join(sorted(data))
+    phytozomedler.export_results(sorted_data, path)
 
-my_query = \
-    phytozomedler.make_my_xml([['organism_id', '447'], ['has_ko', 'excluded="0"']],
-                               ['gene_name1', 'transcript_name1', 'kog_id',
-                                'kog_desc', 'kegg_enzyme_id', 'kegg_enzyme_desc',
-                                'ko_id'], 'GFF', 'af')
-
-data = phytozomedler.get_results(my_query)
-phytozomedler.export_results(data, 'arab_kegg.GFF')
-
-r = random.randint(0, len(data.split('\n')))
+entries = len(data) - 1
+print(entries)
+r = random.randint(0, entries)
 print (r)
-print(data.split('\n')[r]) #.split('\t')[3]
+my_entry = data[r]
+print(my_entry)
+kegg_id = my_entry.strip()
+net_query = \
+    phytozomedler.make_my_xml([['kegg_orth_list', kegg_id]],
+                              ['organism_name'], 'TSV', 'af')
+kegg_specs = phytozomedler.get_results(net_query)
+phytozomedler.export_results(kegg_specs, 'species_with_KEGG_{}.TSV'.format(kegg_id))
+species_num = len(kegg_specs.split('\n')) - 1
+print ('Kegg ID {} data is available for {} species on Phytozome.'.format(
+    kegg_id, species_num))
+
